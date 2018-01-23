c2f94da6fda6d11c7388b68792eb5e118674d1f3
Jc Din <jcdin@channelfix.com>
2018-01-23 09:43:35 +0800

    fix incorrect sanic app config, update runtime to use the latest heroku supported python lib

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-from app.settings import app
+from app.settings import app, RUNNER_CONFIG
 
 if __name__ == '__main__':
     from app import urls
-    app.run()
+    app.run(**RUNNER_CONFIG)
