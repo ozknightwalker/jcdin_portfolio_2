54032ef31c80702a16c9997fc85ff8af4bf2c8ff
Jc Din <jcdin@channelfix.com>
2018-01-23 08:16:49 +0800

    add Procfile and runtime.txt file and update `app/settings.py`
    Procfile will declare the tasks for the heroku dynos to work on and runtime.txt just contains
    what Python version to use.
    
    Update app config to use environment variables

--- app/settings.py
+++ app/settings.py
@@ -1,13 +1,16 @@
 from __future__ import unicode_literals
 
 import os
+from multiprocessing import cpu_count
 
 from sanic import Sanic
 
 app = Sanic(__name__)
 
 app.config.from_object(dict(
-    KEEP_ALIVE=False,
+    KEEP_ALIVE=os.environ.get('KEEP_ALIVE', True),
     HOST=os.environ.get('HOST', '0.0.0.0'),
     PORT=os.environ.get('PORT', 8000),
+    WORKERS=os.environ.get('WORKERS', cpu_count()),
+    DEBUG=os.environ.get('DEBUG', False),
 ))
