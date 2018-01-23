5f8ea36ff1669dfc675bba700cfb7dee861c2451
Jc Din <jcdin@channelfix.com>
2018-01-23 10:15:20 +0800

    add gzip compressor for sanic requests

--- app/settings.py
+++ app/settings.py
@@ -5,10 +5,15 @@ from multiprocessing import cpu_count
 
 from sanic import Sanic
 
+from app.core.compressor import Compress
+
 app = Sanic(__name__)
+Compress(app)
 
 app.config.from_object(dict(
     KEEP_ALIVE=bool(os.environ.get('KEEP_ALIVE', False)),
+    COMPRESS_LEVEL=int((os.environ.get('COMPRESS_LEVEL', 6))),
+    COMPRESS_MIN_SIZE=int((os.environ.get('COMPRESS_MIN_SIZE', 500))),
 ))
 
 RUNNER_CONFIG = {
